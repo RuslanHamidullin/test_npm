7db16bff515b2cfe3089ca267bf320c29fb57aa5
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-06-24 17:57:49 +0300

    Initiall commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var Q = require('kew');
+var pg = require('pg');
+var follow = require('follow');
+var EventEmitter = require('events').EventEmitter;
+var async = require('async');
+var Q = require('kew');
+var pgescape = require('pg-escape');
+
+console.log("cool")
