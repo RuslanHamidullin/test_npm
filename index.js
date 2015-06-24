668251a5d57f0a92fdf8b193b3e491097e8447a6
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-06-24 19:46:59 +0300

    Third commit

--- index.js
+++
@@ -1,9 +0,0 @@
-var Q = require('kew');
-var pg = require('pg');
-var follow = require('follow');
-var EventEmitter = require('events').EventEmitter;
-var async = require('async');
-var Q = require('kew');
-var pgescape = require('pg-escape');
-
-console.log("cool")
